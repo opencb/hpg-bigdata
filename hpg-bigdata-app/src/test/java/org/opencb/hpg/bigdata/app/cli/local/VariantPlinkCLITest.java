43b95a0a057f13d1071f85a5ac16a06b6337dc8f
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2017-06-14 12:34:30 +0100

    test: update some tests

--- hpg-bigdata-app/src/test/java/org/opencb/hpg/bigdata/app/cli/local/VariantPlinkCLITest.java
+++ hpg-bigdata-app/src/test/java/org/opencb/hpg/bigdata/app/cli/local/VariantPlinkCLITest.java
@@ -3,6 +3,8 @@ package org.opencb.hpg.bigdata.app.cli.local;
 import org.apache.htrace.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.sql.*;
 import org.junit.Test;
 import org.opencb.biodata.models.variant.Variant;
@@ -13,6 +15,7 @@ import org.opencb.hpg.bigdata.core.lib.VariantDataset;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Iterator;
 
 /**
  * Created by joaquin on 1/19/17.
@@ -60,14 +63,29 @@ public class VariantPlinkCLITest {
 
         vd.load(inPath.toString());
         vd.createOrReplaceTempView("vcf");
-        String sql = "select * from vcf limit 1";
+        String sql = "select * from vcf";
         Dataset<Row> result = vd.sqlContext().sql(sql);
+
+
+        JavaPairRDD<String, Iterable<Row>> rdd = result.toJavaRDD()
+                .groupBy(row -> row.getString(row.fieldIndex("chromosome")));
+        System.out.println(rdd.collect());
+/*
+        Encoder encoder = null;
+        Encoder<Variant> variantEncoder = Encoders.bean(Variant.class);
+        Dataset<Variant> dv = result.flatMap(new FlatMapFunction<Row, Variant>() {
+            @Override
+            public Iterator<Variant> call(Row row) throws Exception {
+                return null;
+            }
+        }, variantEncoder).as(variantEncoder);
 
         String json = result.toJSON().head();
         ObjectMapper mapper = new ObjectMapper();
         Variant variant = mapper.readValue(json, Variant.class);
         System.out.println(variant.toString());
         System.out.println(variant.toJson());
+*/
     }
 
 }
