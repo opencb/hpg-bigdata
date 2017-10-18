2472dee34722da01552cff3c05d54e3f8c1179a6
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2017-10-18 11:43:46 +0100

    core: add version to the analysis configuration #133

--- hpg-bigdata-core/src/main/java/org/opencb/hpg/bigdata/core/config/AnalysisConfiguration.java
+++ hpg-bigdata-core/src/main/java/org/opencb/hpg/bigdata/core/config/AnalysisConfiguration.java
@@ -3,30 +3,49 @@ package org.opencb.hpg.bigdata.core.config;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AnalysisConfiguration {
+public class AnalysisConfiguration extends HashMap<String, AnalysisConfiguration.Analysis> {
 
-    // Map for binary paths, e.g.: (key, value) = ("plink", "~/softs/plink")
-    private Map<String, String> binaries;
+    public static class Analysis {
+        private String path;
+        private String version;
+
+        public Analysis() {
+        }
+
+        public Analysis(String path, String version) {
+            this.path = path;
+            this.version = version;
+        }
+
+        @Override
+        public String toString() {
+            return "Analysis{" + "path='" + path + '\'' + ", version='" + version + '\'' + '}';
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public Analysis setPath(String path) {
+            this.path = path;
+            return this;
+        }
+
+        public String getVersion() {
+            return version;
+        }
+
+        public Analysis setVersion(String version) {
+            this.version = version;
+            return this;
+        }
+    }
 
     public AnalysisConfiguration() {
-        this(new HashMap<>());
+        super(new HashMap<>());
     }
 
-    public AnalysisConfiguration(Map<String, String> binaries) {
-        this.binaries = binaries;
-    }
-
-    @Override
-    public String toString() {
-        return "AnalysisConfiguration{"
-                + "binaries=" + binaries + '}';
-    }
-
-    public Map<String, String> getBinaries() {
-        return binaries;
-    }
-
-    public void setBinaries(Map<String, String> binaries) {
-        this.binaries = binaries;
+    public AnalysisConfiguration(Map<String, Analysis> analysis) {
+        super(analysis);
     }
 }
