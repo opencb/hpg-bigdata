e1c2bfd41b7f9eaba6b5e8b69b084361a9be9cde
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2017-10-18 14:29:18 +0100

    core: update configuration test

--- hpg-bigdata-core/src/test/java/org/opencb/hpg/bigdata/core/config/OskarConfigurationTest.java
+++ hpg-bigdata-core/src/test/java/org/opencb/hpg/bigdata/core/config/OskarConfigurationTest.java
@@ -7,6 +7,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OskarConfigurationTest {
 
@@ -14,11 +16,17 @@ public class OskarConfigurationTest {
     public void testDefault() throws Exception {
         OskarConfiguration storageConfiguration = new OskarConfiguration();
 
-//
         CellBaseConfiguration cellBaseConfiguration = new CellBaseConfiguration(Arrays.asList("localhost"), "v3", new DatabaseCredentials
                 (Arrays.asList("localhost"), "user", "password"));
-
         storageConfiguration.setCellbase(cellBaseConfiguration);
+
+        AnalysisConfiguration.Analysis plink = new AnalysisConfiguration.Analysis("~/soft/plink", "1.9");
+        AnalysisConfiguration.Analysis rvtests = new AnalysisConfiguration.Analysis("~/soft/rvtests", "2017");
+        Map<String, AnalysisConfiguration.Analysis> analysisMap = new HashMap<>();
+        analysisMap.put("plink", plink);
+        analysisMap.put("rvtests", rvtests);
+        storageConfiguration.setAnalysis(new AnalysisConfiguration(analysisMap));
+
         File file = Paths.get("/tmp/configuration.yml").toFile();
         try (FileOutputStream os = new FileOutputStream(file)) {
             storageConfiguration.serialize(os);
