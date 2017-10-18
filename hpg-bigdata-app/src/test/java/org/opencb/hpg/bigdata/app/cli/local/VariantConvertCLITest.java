e1bb88a0e0da42bea1be23589092e623f1c9c6bf
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2017-10-18 11:45:35 +0100

    app: clean junit test for the variants convert CLI

--- hpg-bigdata-app/src/test/java/org/opencb/hpg/bigdata/app/cli/local/VariantConvertCLITest.java
+++ hpg-bigdata-app/src/test/java/org/opencb/hpg/bigdata/app/cli/local/VariantConvertCLITest.java
@@ -3,7 +3,6 @@ package org.opencb.hpg.bigdata.app.cli.local;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,28 +15,49 @@ public class VariantConvertCLITest {
     public Path vcfPath;
     public Path pedPath;
     public Path avroPath;
+    public Path metaAvroPath;
     public Path parquetPath;
-    public Path metaPath;
+    public Path metaParquetPath;
 
     @Before
     public void init() throws URISyntaxException {
-//        vcfPath = Paths.get(getClass().getResource("/test.vcf").toURI());
-//        pedPath = Paths.get(getClass().getResource("/test.ped").toURI());
-//        avroPath = Paths.get("/tmp/test.vcf.avro");
-//        parquetPath = Paths.get("/tmp/test.vcf.parquet");
-//        metaPath = Paths.get("/tmp/test.vcf.avro.meta.json");
+        String vcfFilename = "../hpg-bigdata-core/src/test/resources/org/opencb/hpg/bigdata/core/utils/VariantContextBlockIteratorTest.vcf";
+        //String vcfFilename = "../hpg-bigdata-core/src/test/resources/org/opencb/hpg/bigdata/core/utils/VcfBlockIteratorTest.vcf";
+        //String vcfFilename = "~/data150/vcf/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf";
+        //String vcfFilename = "~/data150/vcf/2.vcf";
+        //String vcfFilename = "~/data150/vcf/5k.vcf";
+        //String vcfFilename = "~/data150/vcf/100k.vcf";
+        vcfPath = Paths.get(vcfFilename);
+        Path parquetPath = Paths.get("/tmp/" + vcfPath.getFileName() + ".parquet");
+
+        vcfPath = Paths.get(vcfFilename);
+        avroPath = Paths.get("/tmp/" + vcfPath.getFileName() + ".avro");
+        metaAvroPath = Paths.get(avroPath + ".meta.json");
+        parquetPath = Paths.get("/tmp/" + vcfPath.getFileName() + ".parquet");
+        metaParquetPath = Paths.get(parquetPath + ".meta.json");
+
+        // Delete output files before testing
+        deleteFiles();
     }
 
-    //@Test
+    public void deleteFiles() {
+        if (avroPath != null) { avroPath.toFile().delete(); }
+        if (metaAvroPath != null) { metaAvroPath.toFile().delete(); }
+        if (parquetPath != null) { parquetPath.toFile().delete(); }
+        if (metaParquetPath != null) { metaParquetPath.toFile().delete(); }
+    }
+
+    @Test
     public void vcf2avro() {
         try {
             StringBuilder commandLine = new StringBuilder();
-            commandLine.append(" variant convert");
+            commandLine.append("variant convert");
             commandLine.append(" --log-level ERROR");
             commandLine.append(" -i ").append(vcfPath);
             commandLine.append(" -o ").append(avroPath);
             commandLine.append(" --to avro");
             commandLine.append(" --dataset test-dataset");
+            commandLine.append(" -t 1");
 
             VariantQueryCLITest.execute(commandLine.toString());
         } catch (Exception e) {
@@ -47,22 +67,15 @@ public class VariantConvertCLITest {
 
     @Test
     public void vcf2parquet() {
-        //String vcfFilename = "../hpg-bigdata-core/src/test/resources/org/opencb/hpg/bigdata/core/utils/VariantContextBlockIteratorTest.vcf";
-        String vcfFilename = "../hpg-bigdata-core/src/test/resources/org/opencb/hpg/bigdata/core/utils/VcfBlockIteratorTest.vcf";
-        vcfPath = Paths.get(vcfFilename);
-        parquetPath = Paths.get("/tmp/" + vcfPath.getFileName() + ".parquet");
-
-        new File(parquetPath.toString()).delete();
-        new File(parquetPath.toString() + ".meta.json").delete();
-
         try {
             StringBuilder commandLine = new StringBuilder();
-            commandLine.append(" variant convert");
+            commandLine.append("variant convert");
             commandLine.append(" --log-level ERROR");
             commandLine.append(" -i ").append(vcfPath);
             commandLine.append(" -o ").append(parquetPath);
             commandLine.append(" --to parquet");
             commandLine.append(" --dataset test-dataset");
+            commandLine.append(" -t 1");
 
             VariantQueryCLITest.execute(commandLine.toString());
         } catch (Exception e) {
@@ -70,17 +83,20 @@ public class VariantConvertCLITest {
         }
     }
 
-    //@Test
+    @Test
     public void avro2parquet() {
         try {
+            vcf2avro();
+
             StringBuilder commandLine = new StringBuilder();
-            commandLine.append(" variant convert");
+            commandLine.append("variant convert");
             commandLine.append(" --log-level ERROR");
             commandLine.append(" -i ").append(avroPath);
             commandLine.append(" -o ").append(parquetPath);
             commandLine.append(" --from avro");
             commandLine.append(" --to parquet");
             commandLine.append(" --dataset test-dataset");
+            commandLine.append(" -t 1");
 
             VariantQueryCLITest.execute(commandLine.toString());
         } catch (Exception e) {
@@ -90,21 +106,15 @@ public class VariantConvertCLITest {
 
     @Test
     public void avroAnnotate() {
-        String vcfFilename = "../hpg-bigdata-core/src/test/resources/org/opencb/hpg/bigdata/core/utils/VariantContextBlockIteratorTest.vcf";
-        vcfPath = Paths.get(vcfFilename);
-        avroPath = Paths.get("/tmp/" + vcfPath.getFileName() + ".avro");
-
-        new File(avroPath.toString()).delete();
-        new File(avroPath.toString() + ".meta.json").delete();
-
         try {
             StringBuilder commandLine = new StringBuilder();
-            commandLine.append(" variant convert");
+            commandLine.append("variant convert");
             commandLine.append(" --log-level ERROR");
             commandLine.append(" -i ").append(vcfPath);
             commandLine.append(" -o ").append(avroPath);
             commandLine.append(" --dataset test-dataset");
             commandLine.append(" --annotate");
+            commandLine.append(" -t 1");
 
             VariantQueryCLITest.execute(commandLine.toString());
         } catch (Exception e) {
@@ -114,16 +124,9 @@ public class VariantConvertCLITest {
 
     @Test
     public void avroAnnotateParallel() {
-        String vcfFilename = "../hpg-bigdata-core/src/test/resources/org/opencb/hpg/bigdata/core/utils/VariantContextBlockIteratorTest.vcf";
-        vcfPath = Paths.get(vcfFilename);
-        avroPath = Paths.get("/tmp/" + vcfPath.getFileName() + ".avro");
-
-        new File(avroPath.toString()).delete();
-        new File(avroPath.toString() + ".meta.json").delete();
-
         try {
             StringBuilder commandLine = new StringBuilder();
-            commandLine.append(" variant convert");
+            commandLine.append("variant convert");
             commandLine.append(" --log-level ERROR");
             commandLine.append(" -i ").append(vcfPath);
             commandLine.append(" -o ").append(avroPath);
@@ -139,16 +142,9 @@ public class VariantConvertCLITest {
 
     @Test
     public void parquetAnnotate() {
-        String vcfFilename = "../hpg-bigdata-core/src/test/resources/org/opencb/hpg/bigdata/core/utils/VariantContextBlockIteratorTest.vcf";
-        vcfPath = Paths.get(vcfFilename);
-        Path parquetPath = Paths.get("/tmp/" + vcfPath.getFileName() + ".parquet");
-
-        new File(parquetPath.toString()).delete();
-        new File(parquetPath.toString() + ".meta.json").delete();
-
         try {
             StringBuilder commandLine = new StringBuilder();
-            commandLine.append(" variant convert");
+            commandLine.append("variant convert");
             commandLine.append(" --log-level ERROR");
             commandLine.append(" -i ").append(vcfPath);
             commandLine.append(" -o ").append(parquetPath);
@@ -164,20 +160,9 @@ public class VariantConvertCLITest {
 
     @Test
     public void parquetAnnotateParallel() {
-        String vcfFilename = "../hpg-bigdata-core/src/test/resources/org/opencb/hpg/bigdata/core/utils/VariantContextBlockIteratorTest.vcf";
-        //String vcfFilename = "/home/jtarraga/data150/vcf/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf";
-        //String vcfFilename = "/home/jtarraga/data150/vcf/2.vcf";
-        //String vcfFilename = "/home/jtarraga/data150/vcf/5k.vcf";
-        //String vcfFilename = "/home/jtarraga/data150/vcf/100k.vcf";
-        vcfPath = Paths.get(vcfFilename);
-        Path parquetPath = Paths.get("/tmp/" + vcfPath.getFileName() + ".parquet");
-
-        new File(parquetPath.toString()).delete();
-        new File(parquetPath.toString() + ".meta.json").delete();
-
         try {
             StringBuilder commandLine = new StringBuilder();
-            commandLine.append(" variant convert");
+            commandLine.append("variant convert");
             commandLine.append(" --log-level ERROR");
             commandLine.append(" -i ").append(vcfPath);
             commandLine.append(" -o ").append(parquetPath);
