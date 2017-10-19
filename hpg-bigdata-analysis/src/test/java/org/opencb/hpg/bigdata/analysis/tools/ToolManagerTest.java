937b37e7377120b61c203650b5ccf228489ab804
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2017-10-19 11:24:30 +0100

    analysis: fix tool manager test (samtools pemission issue)

--- hpg-bigdata-analysis/src/test/java/org/opencb/hpg/bigdata/analysis/tools/ToolManagerTest.java
+++ hpg-bigdata-analysis/src/test/java/org/opencb/hpg/bigdata/analysis/tools/ToolManagerTest.java
@@ -2,11 +2,14 @@ package org.opencb.hpg.bigdata.analysis.tools;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.opencb.hpg.bigdata.analysis.exceptions.AnalysisToolException;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -20,26 +23,41 @@ import static org.junit.Assert.assertTrue;
  */
 public class ToolManagerTest {
 
+    private Path tmpFolder;
+    private Path toolPath;
+
+    @Before
+    public void setUp() throws Exception {
+        tmpFolder = Paths.get("target/test-data/junit-" + RandomStringUtils.randomAlphabetic(10));
+        Files.createDirectories(tmpFolder);
+
+        toolPath = tmpFolder.resolve("tools");
+        Files.createDirectories(toolPath.resolve("samtools"));
+
+        Files.copy(Paths.get(getClass().getResource("/tools/samtools/manifest.json").toURI()), toolPath.resolve("samtools").resolve("manifest.json"));
+        Files.copy(Paths.get(getClass().getResource("/tools/samtools/samtools").toURI()), toolPath.resolve("samtools").resolve("samtools"));
+
+        toolPath.resolve("samtools").resolve("samtools").toFile().setExecutable(true);
+    }
+
     @Test
     public void testToolManager() throws URISyntaxException, AnalysisToolException, IOException {
-        Path toolPath = Paths.get(getClass().getResource("/tools").toURI());
         Path testBam = Paths.get(getClass().getResource("/test.bam").toURI());
-        Path tmp = Paths.get("/tmp");
 
         ToolManager toolManager = new ToolManager(toolPath);
         Map<String, String> params = new HashMap<>();
         params.put("input", testBam.toAbsolutePath().toString());
-        params.put("output", "/tmp/test.bam.bai");
+        params.put("output", tmpFolder.resolve("test.bam.bai").toString());
 
         String commandLine = toolManager.createCommandLine("samtools", "index", params);
         System.out.println(commandLine);
-        Executor.execute(commandLine, tmp, true);
+        Executor.execute(commandLine, tmpFolder, true);
 
         ObjectReader reader = new ObjectMapper().reader(Status.class);
-        Status status = reader.readValue(tmp.resolve("status.json").toFile());
+        Status status = reader.readValue(tmpFolder.resolve("status.json").toFile());
         assertEquals(Status.DONE, status.getName());
 
-        assertTrue(tmp.resolve("test.bam.bai").toFile().exists());
+        assertTrue(tmpFolder.resolve("test.bam.bai").toFile().exists());
     }
 
 }
